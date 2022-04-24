c71b76e4889e97115791c70bb296c68e212276e9
Flux State <f1ux.state@pm.me>
2022-04-24 16:00:19 +0200

    targets cleanup and update

--- internal/targets/websites.go
+++ internal/targets/websites.go
@@ -101,7 +101,7 @@ var TargetWebsites = map[string]struct{}{
 	"https://novorossiia.ru":              {}, // cf
 	"https://novorossiia.info":            {},
 	"http://novorossy.ru":                 {},
-	"https://novosti.icu": {},
+	"https://novosti.icu":                 {},
 	"http://novosti333.ru":                {},
 	"https://nt1941.su":                   {},
 	"https://pandoraopen.ru":              {},
@@ -117,6 +117,7 @@ var TargetWebsites = map[string]struct{}{
 	"https://www.ritmeurasia.org":         {},
 	"https://rodina.news":                 {}, // cf
 	"https://rossaprimavera.ru":           {},
+	"https://www.company.rt.ru":           {},
 	"https://rusdnepr.ru":                 {},
 	"https://rusnext.ru":                  {}, // cf
 	"https://rusnext.ru/newslistapi.json": {}, // cf
@@ -165,108 +166,84 @@ var TargetWebsites = map[string]struct{}{
 	"https://www.voltairenet.org":               {}, // cf
 
 	// Business corporations
-	"https://www.evraz.com/ru/":                            {},
-	"https://www.wildberries.ru":                           {},
-	"https://www.ozon.ru":                                  {},
-	"https://www.ozon.ru/api/composer-api.bx/page/json/v2": {},
-	"https://www.dns-shop.ru":                              {},
-	"https://advego.com":                                   {},
-	"https://freelance.ru":                                 {},
-	"https://www.turbotext.ru":                             {},
-	"https://www.ingosinvest.ru":                           {},
-	"https://new.ingosinvest.ru/Registration/Info":         {},
-	"https://pif.ingosinvest.ru":                           {},
-	"https://ok.ru":                                        {},
-	"http://5.61.23.11":                                    {},
-	"http://217.20.155.13":                                 {},
-	"http://217.20.147.1":                                  {},
-	"http://www.yemelya.ru":                                {},
-	"https://vcs.rostec.ru":                                {},
-	"https://vks3.rostec.ru":                               {},
-	"https://kontur.ru":                                    {},
-	"http://app.rzd.ru":                                    {},
-	"https://team.rzd.ru":                                  {},
-	"https://rzdint.ru":                                    {},
-	"https://www.rzdlog.com":                               {},
-
-	// Banks
-	"https://sber.ru": {},
-	"https://promo.sber.ru/sberserenity":               {},
-	"https://sberprime.sber.ru":                        {},
-	"https://www.sberbank.com/ru":                      {},
-	"https://www.sberbank.ru":                          {},
-	"https://api.sberbank.ru/prod/tokens/v2":           {},
-	"https://api.sberbank.ru/prod/tokens/v2/oauth":     {},
-	"https://api.sberbank.ru/prod/tokens/v2/oidc":      {},
-	"https://online.sberbank.ru/CSAFront/index.do":     {},
-	"https://cl.vtb.ru":                                {},
-	"https://ipoteka.vtb.ru/ipoteka/1":                 {},
-	// 	"https://online.vtb.ru/login": {}, // TODO small read buffer
-	"https://school.vtb.ru":                                          {},
-	"https://www.moex.com":                                           {},
-	"https://iss.moex.com/iss/reference/":                            {},
-	"https://messaging.moex.com/init":                                {},
-	"https://passport.moex.com/login":                                {},
-	"http://www.fsb.ru":                                              {},
-	"https://scr.online.sberbank.ru/api/fl/idgib-w-3ds":              {},
-	"https://acs1.sbrf.ru":                                           {},
-	"https://acs2.sbrf.ru":                                           {},
-	"https://acs3.sbrf.ru":                                           {},
-	"https://acs4.sbrf.ru":                                           {},
-	"https://acs5.sbrf.ru":                                           {},
-	"https://acs6.sbrf.ru":                                           {},
-	"https://acs7.sbrf.ru":                                           {},
-	"https://acs8.sbrf.ru":                                           {},
-	"https://online.gpb.ru/login":                                    {},
+	"http://www.crimearw.ru":                                      {},
+	"https://www.wildberries.ru":                                  {},
+	"https://api-stories.wildberries.ru/api/v1/articles/previews": {},
+	"https://chat.wildberries.ru/v1/unauth/messages":              {},
+	"https://guru.wildberries.ru":                                 {},
+	"https://suggestions.wildberries.ru/api/v2/hint":              {},
+	"https://wbxcatalog-ru.wildberries.ru/tops_tshirts/catalog":   {},
+	"https://wbxsearch.wildberries.ru/exactmatch/v2/common":       {},
+	"https://www.ozon.ru":                                         {},
+	"https://www.ozon.ru/api/composer-api.bx/page/json/v2":        {},
+	"https://brandlab.ozon.ru":                                    {},
+	"https://docs.ozon.ru/main":                                   {},
+	"https://job.ozon.ru":                                         {},
+	"https://seller.ozon.ru":                                      {},
+	"https://www.dns-shop.ru":                                     {},
+	"https://advego.com":                                          {},
+	"https://freelance.ru":                                        {},
+	"https://new.ingosinvest.ru/Registration/Info":                {},
+	"https://pif.ingosinvest.ru":                                  {},
+	"https://ok.ru":                                               {},
+	"http://www.yemelya.ru":                                       {},
+	"https://vcs.rostec.ru":                                       {},
+	"https://vks3.rostec.ru":                                      {},
+	"https://kontur.ru":                                           {},
+	"http://app.rzd.ru":                                           {},
+	"https://team.rzd.ru":                                         {},
+	"https://rzdint.ru":                                           {},
+	"https://www.rzdlog.com":                                      {},
 
 	//The state
-	"https://mil.ru":                              {},
-	"https://www.nalog.gov.ru":                    {}, // should be called without params
-	"https://pfr.gov.ru":                          {},
-	"https://gosuslugi41.ru":                      {},
-	"https://otpravka.pochta.ru":                  {},
-	"https://passport.pochta.ru/oauth2/authorize": {},
-	"https://tariff.pochta.ru":                    {},
-	"https://zakaznoe.pochta.ru":                  {},
-	"http://crimea-post.ru":                       {},
-	"https://ca.vks.rosguard.gov.ru":              {},
-	"https://sozd.duma.gov.ru":                    {},
-	"https://vsednr.ru":                           {},
-	"https://ecis.admoblkaluga.ru":                       {},
-	"https://entry.admoblkaluga.ru/Web/Login":            {},
-	"https://mimz.admoblkaluga.ru/Menu/Page/1" :          {},
-	"https://mimz.admoblkaluga.ru/GzwSP/ContractsJson" :  {},
-	"https://pre.admoblkaluga.ru/main/":                  {},
-	"https://sadko30.admoblkaluga.ru/account/login":      {},
-	"https://soc.admoblkaluga.ru":                        {},
-	"https://spo.admoblkaluga.ru/security/":              {},
-	"https://tender.admoblkaluga.ru":                     {},
-	"https://dnmchs.ru":                                  {},
-	"https://govdnr.ru":                                  {},
-	"https://mer.govdnr.ru":                              {},
-	"https://mid-dnr.su/ru/":                             {},
-	"https://agmo.mosreg.ru":                             {},
-	"https://br.mosreg.ru/login/auth/":                   {},
-	"https://dk.mosreg.ru":                               {},
-	"https://easuz.mosreg.ru":                            {},
-	"https://gasu.mosreg.ru":                             {},
-	"https://invest.mosreg.ru":                           {},
-	"https://pass.mosreg.ru/login":                       {},
-	"https://login.school.mosreg.ru":                     {},
-	"https://web.mail.mosreg.ru/SOGo/":                   {},
-	"https://market.mosreg.ru":                           {},
-	"https://rgis.mosreg.ru/v3/":                         {},
-	"https://sso.mosreg.ru/signin.jsp":                   {},
-	"https://support.mosreg.ru":                          {},
-	"https://uslugi.mosreg.ru":                           {},
-	"https://vmeste.mosreg.ru/login":                     {},
-	"https://welcome.mosreg.ru":                          {},
-	"http://wow.mosreg.ru/admin/":                        {},
-	"https://edu-mosreg.ru":                              {},
-	"https://mzdnr.ru":                                   {},
-	"https://pravdnr.ru":                                 {},
-	"https://supcourt-dpr.su":                            {},
-	"https://ulgov.ru":                                   {},
+	"https://mil.ru":                                   {},
+	"https://www.nalog.gov.ru":                         {}, // should be called without params
+	"https://pfr.gov.ru":                               {},
+	"https://gosuslugi41.ru":                           {},
+	"https://otpravka.pochta.ru":                       {},
+	"https://passport.pochta.ru/oauth2/authorize":      {},
+	"https://tariff.pochta.ru":                         {},
+	"https://zakaznoe.pochta.ru":                       {},
+	"http://crimea-post.ru":                            {},
+	"https://ca.vks.rosguard.gov.ru":                   {},
+	"https://sozd.duma.gov.ru":                         {},
+	"https://vsednr.ru":                                {},
+	"https://ecis.admoblkaluga.ru":                     {},
+	"https://entry.admoblkaluga.ru/Web/Login":          {},
+	"https://mimz.admoblkaluga.ru/Menu/Page/1":         {},
+	"https://mimz.admoblkaluga.ru/GzwSP/ContractsJson": {},
+	"https://pre.admoblkaluga.ru/main/":                {},
+	"https://sadko30.admoblkaluga.ru/account/login":    {},
+	"https://soc.admoblkaluga.ru":                      {},
+	"https://spo.admoblkaluga.ru/security/":            {},
+	"https://tender.admoblkaluga.ru":                   {},
+	"https://dnmchs.ru":                                {},
+	"http://www.fsb.ru":                                {},
+	"https://govdnr.ru":                                {},
+	"https://mer.govdnr.ru":                            {},
+	"https://mid-dnr.su/ru/":                           {},
+	"https://agmo.mosreg.ru":                           {},
+	"https://br.mosreg.ru/login/auth/":                 {},
+	"https://dk.mosreg.ru":                             {},
+	"https://easuz.mosreg.ru":                          {},
+	"https://gasu.mosreg.ru":                           {},
+	"https://invest.mosreg.ru":                         {},
+	"https://pass.mosreg.ru/login":                     {},
+	"https://login.school.mosreg.ru":                   {},
+	"https://web.mail.mosreg.ru/SOGo/":                 {},
+	"https://market.mosreg.ru":                         {},
+	"https://rgis.mosreg.ru/v3/":                       {},
+	"https://sso.mosreg.ru/signin.jsp":                 {},
+	"https://support.mosreg.ru":                        {},
+	"https://uslugi.mosreg.ru":                         {},
+	"https://vmeste.mosreg.ru/login":                   {},
+	"https://welcome.mosreg.ru":                        {},
+	"http://wow.mosreg.ru/admin/":                      {},
+	"https://edu-mosreg.ru":                            {},
+	"https://mzdnr.ru":                                 {},
+	"https://pravdnr.ru":                               {},
+	"https://supcourt-dpr.su":                          {},
+	"https://ulgov.ru":                                 {},
 
 	// Embassies
 	// Do not duplicate the IPs: https://github.com/erkexzcx/stoppropaganda/pull/110#issuecomment-1059960305
@@ -274,52 +251,11 @@ var TargetWebsites = map[string]struct{}{
 	"https://belarus.mid.ru":  {},
 
 	// Others
-	"https://proverki-gov.ru":    {},
 	"https://www.glonass-iac.ru": {},
-	"https://market.dme.ru":      {},
-	"https://www.tickets.dme.ru": {},
-
-	// Payments
-	"http://185.170.2.7":         {},
-	"http://185.170.3.7":         {},
-	"https://wats.mirconnect.ru": {},
-	"https://passport.etpgpb.ru": {},
-	"https://bki-okb.ru":         {},
-
-	// Exchanges connected to russian banks
-	"https://bitokk.biz":      {},
-	"https://www.netex24.net": {}, // requires cookie through redirect to fully load
-	"https://flashobmen.com":  {},
 
 	// Electronic signature services, certificate authorities, www domain names
-	"https://kb.kubankredit.ru/login":     {},
-	"https://structure.mil.ru":            {},
-	"https://parus-s.ru":                  {},
-	"https://www.icentr.ru":               {},
 	"https://etp.kartoteka.ru/index.html": {},
-	"https://www.cit-ufa.ru":              {},
-	"https://api.cit-ufa.ru":              {},
-	"https://icvibor.ru":                  {},
-	"https://1c.icvibor.ru":               {},
 	"http://mcspro.ru":                    {},
-	"https://iecp.ru/ep/ep-verification":  {},
-	"https://e-trust.gosuslugi.ru":        {},
-	"https://gu.spb.ru":                   {}, // should be called without params
-	"https://iecp.ru/ep/uc-list":          {},
-
-	// Oil and gas trading companies
-	"https://www.tektorg.ru":                                      {},
-	"https://44.tektorg.ru/authentication/login":                  {},
-	"https://fin.tektorg.ru/bg":                                   {},
-	"https://fin.tektorg.ru/api/v2/reference/service_types":       {},
-	"https://zakupki.tektorg.ru":                                  {},
-	"https://aaa-srt-cs.lukoil.com/logon/LogonPoint/tmindex.html": {},
-	"https://ar.lukoil.com/login":                                 {},
-	"https://b2b.sibur.ru/pages_new_en/faces/index.jspx":          {},
-	"https://onlinecontract.ru":                                   {},
-
-	// Food delivery services
-	"https://samokat.ru":    {},
 
 	// Cinemas
 	"https://karofilm.ru":   {},
@@ -339,11 +275,10 @@ var TargetWebsites = map[string]struct{}{
 	"https://perevozki31.ru":                                   {},
 
 	// Others
-	"http://217.12.104.100":  {},
+	"http://217.12.104.100": {},
 
 	// Various websites by ip
 	"https://213.59.254.8":   {},
-	"https://213.59.197.65":  {},
 	"https://82.151.111.187": {},
 
 	/* BELARUS */
@@ -360,10 +295,10 @@ var TargetWebsites = map[string]struct{}{
 	"https://www.rw.by": {},
 
 	// by media
-	"https://www.belta.by":        {},
-	"https://www.belarus.by/ru/":  {},
-	"https://ont.by":              {}, // should be called without params
-	"https://www.belnovosti.by":   {},
+	"https://www.belta.by":       {},
+	"https://www.belarus.by/ru/": {},
+	"https://ont.by":             {}, // should be called without params
+	"https://www.belnovosti.by":  {},
 
 	/* Syria */
 	"https://syrianfinance.gov.sy": {},
@@ -391,54 +326,12 @@ var TargetWebsites = map[string]struct{}{
 	"https://www.yves-rocher.ru":                                    {},
 
 	// https://t.me/itarmyofukraine2022/230
-	"https://www.mid.ru":         {},
+	"https://www.mid.ru": {},
 
 	// https://t.me/itarmyofukraine2022/215
 	"https://www.chechnya.online": {},
 	"https://vaynahavia.com":      {},
 	"https://www.minfinchr.ru":    {},
-
-	"https://www.company.rt.ru": {},
-
-	// https://t.me/itarmyofukraine2022/243
-	"https://fss.gov.ru":              {},
-	"https://autodiscover.fss.ru/owa": {},
-	"https://portal.fss.ru":           {},
-	"https://data.fss.ru/open":        {},
-	"http://docs.fss.ru":              {},
-	"https://hosting.pfrf.ru":         {},
-	"https://school.pfr.gov.ru":       {},
-	"https://es-pfrf.ru/":             {},
-
-	// https://t.me/itarmyofukraine2022/248
-	"https://digital.ac.gov.ru":       {},
-	"https://platform.digital.gov.ru": {},
-	"https://reestr.digital.gov.ru":   {},
-	"https://sc.digital.gov.ru/home":  {},
-
-	// https://t.me/itarmyofukraine2022/251
-	"https://payanyway.ru/info/w/ru/public/welcome.htm": {},
-
-	// https://t.me/itarmyofukraine2022/269
-	"https://cdek.by/ru/":         {},
-	"https://www.cdek.ru/graphql": {},
-
-	// https://t.me/itarmyofukraine2022/273
-	"https://www.doski.ru":                           {},
-	"https://www.farpost.ru":                         {},
-	"https://unibo.ru":                               {},
-	"https://api.youla.io/api/v1/search/suggestions": {},
-
-	// https://t.me/itarmyofukraine2022/275
-	"https://www.crpt.ru":                  {},
-	"https://ismp.crpt.ru":                 {},
-	"https://kb.crpt.ru":                   {},
-	"https://markirovka.crpt.ru/login-kep": {},
-	"https://api.mdlp.crpt.ru":             {},
-	"https://api.sb.mdlp.crpt.ru":          {},
-	"https://nrz.api.sb.mdlp.crpt.ru":      {},
-	"https://milk.crpt.ru":                 {},
-	"https://suzgrid.crpt.ru":              {},
 
 	// https://t.me/itarmyofukraine2022/287
 	"https://www.amediateka.ru": {},
@@ -447,19 +340,6 @@ var TargetWebsites = map[string]struct{}{
 
 	// https://t.me/itarmyofukraine2022/289
 	"https://stream.1tv.ru/api/playlist/1tvch-v1_as_array.json": {},
-
-	// https://t.me/itarmyofukraine2022/290
-	"https://api.kontur.ru/csi/support/v1/public/integrations/zakupki": {},
-	"https://auth.kontur.ru":                       {},
-	"https://ca.kontur.ru":                         {},
-	"https://developer.kontur.ru":                  {},
-	"https://elbank.kontur.ru/AccessControl/Login": {},
-	// 	"https://focus.kontur.ru":                      {}, // TODO read buffer size
-	"https://focus.kontur.ru/api/lists/all": {},
-	"https://pf.kontur.ru":                  {},
-	"https://zakupki.kontur.ru":             {},
-	"https://www.b-kontur.ru":               {},
-	"https://www.kontur-extern.ru":          {},
 
 	// https://t.me/itarmyofukraine2022/302
 	"https://rov.aero": {},
